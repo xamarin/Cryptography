a38a4a817eb7a0bed57e019ea0e0e4dd2e67abd7
drieseng <gert.driesen@telenet.be>
2016-03-31 18:18:21 +0200

    Reference correct CoreFX issue.

--- src/Renci.Security.Cryptography.UAP10/Properties/AssemblyInfo.cs
+++ src/Renci.Security.Cryptography.UAP10/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Renci.SshNet.Security.Cryptography for UAP 10.0")]
-// https://github.com/dotnet/corefx/issues/7387
+// https://github.com/dotnet/corefx/issues/7274
 //[assembly: Guid("EB0D62D0-F031-4CC3-A294-D7B867DA517B")]
