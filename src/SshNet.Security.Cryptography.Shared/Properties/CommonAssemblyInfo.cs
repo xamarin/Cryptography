816ed11058f0fcab5e9bea92a7438d53373af8eb
Gert Driesen <gertdriesen@msn.com>
2017-10-22 20:50:08 +0200

    Prepare for 1.3.0 release.

--- src/SshNet.Security.Cryptography.Shared/Properties/CommonAssemblyInfo.cs
+++ src/SshNet.Security.Cryptography.Shared/Properties/CommonAssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Cryptographic functions for .NET")]
 [assembly: AssemblyCompany("Renci")]
 [assembly: AssemblyProduct("SshNet.Security.Cryptography")]
-[assembly: AssemblyCopyright("Copyright © Renci 2010-2016")]
+[assembly: AssemblyCopyright("Copyright © Renci 2010-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
-[assembly: AssemblyInformationalVersion("1.2.0")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyInformationalVersion("1.3.0")]
 [assembly: CLSCompliant(false)]
 
 [assembly: ComVisible(false)]
